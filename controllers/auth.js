523d3d4ad68f61324ec79a137edf47025eb3d07e
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-12 10:50:10 -0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-// Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
@@ -9,18 +8,19 @@ passport.use(new BasicStrategy(
     User.findOne({ username: username }, function (err, user) {
       if (err) { return callback(err); }
 
-      // No user found with that username
       if (!user) { return callback(null, false); }
 
-      // Make sure the password is correct
       user.verifyPassword(password, function(err, isMatch) {
+        
+        //Error
         if (err) { return callback(err); }
 
-        // Password did not match
+        // Fail
         if (!isMatch) { return callback(null, false); }
 
         // Success
         return callback(null, user);
+        
       });
     });
   }
