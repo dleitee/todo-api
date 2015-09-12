9cb52b0a11caf2f2ed60ec5a629d75814a9bd82c
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-09-12 10:48:42 -0300

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,11 +1,8 @@
-// Load required packages
 var mongoose = require('mongoose');
 
-// Define our beer schema
 var TodoSchema   = new mongoose.Schema({
   task: String,
   userId: String
 });
 
-// Export the Mongoose model
 module.exports = mongoose.model('Todo', TodoSchema);
